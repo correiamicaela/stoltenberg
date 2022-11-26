c197de13772f121cd1f1237d1026acb97c683cd9
correiamicaela <119089770+correiamicaela@users.noreply.github.com>
2022-11-26 12:26:20 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('https://www.free-css.com/assets/files/free-css-templates/download/page257/chershoee.zip')
-})
-app.listen(process.env.PORT || 3000)
